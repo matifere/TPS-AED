702d17746f489146ad62299468f67cbf96808ba0
matifere <matifere@gmail.com>
2024-11-11 19:27:50 -0300

    creacion de las clases

---
+++ TP2/tp2/src/main/java/aed/Ciudad.java
@@ -0,0 +1,12 @@
+package aed;
+
+public class Ciudad {
+    int id;
+    int gananciaCiudad;
+    int perdidaCiudad;
+    public Ciudad(int id, int gananciaCiudad, int perdidaCiudad){
+        this.id = id;
+        this.gananciaCiudad = gananciaCiudad;
+        this.perdidaCiudad = perdidaCiudad;
+    }
+}
---
+++ TP2/tp2/src/main/java/aed/Heap.java
@@ -0,0 +1,12 @@
+package aed;
+import java.util.ArrayList;
+public class Heap{
+    private ArrayList<Traslado> heap;
+
+    public Heap() {
+        heap = new ArrayList<>();
+    }
+
+    
+
+}
