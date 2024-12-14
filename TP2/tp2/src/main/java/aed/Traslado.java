b5283f80631c98b305ae03b6beb333328a0f2be6
matifere <matifere@gmail.com>
2024-12-14 19:46:32 -0300

    cambio en traslado

--- TP2/tp2/src/main/java/aed/Traslado.java
+++ TP2/tp2/src/main/java/aed/Traslado.java
@@ -8,13 +8,19 @@ public class Traslado implements Comparable<Traslado> {
     int gananciaNeta;
     int timestamp;
 
+    // para los heaps
+    int indiceRed;
+    int indiceAnt;
+
     public Traslado(int id, int origen, int destino, int gananciaNeta, int timestamp) {
         this.id = id;
         this.origen = origen;
         this.destino = destino;
         this.gananciaNeta = gananciaNeta;
         this.timestamp = timestamp;
+
     }
+
 
     public int devolverID() {
         return id;
@@ -36,6 +42,20 @@ public class Traslado implements Comparable<Traslado> {
 
     @Override
     public String toString() {
-        return "Traslado{id=" + id + ", origen=" + origen + ", destino=" + destino + ", ganancia=" + gananciaNeta + ", timestamp=" + timestamp + "}";
+        return "Traslado{id=" + id + ", origen=" + origen + ", destino=" + destino + ", ganancia=" + gananciaNeta
+                + ", timestamp=" + timestamp + "}";
+    }
+
+    public void RedCambiarIndice( int nuevoIndice){
+        indiceRed = nuevoIndice;
+    }
+    public int RedObtener(){
+        return indiceRed;
+    }
+    public void AntCambiarIndice( int nuevoIndice){
+        indiceAnt = nuevoIndice;
+    }
+    public int AntObtener(){
+        return indiceAnt;
     }
 }
